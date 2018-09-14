f8956f689e9bf1e2744c7f84889809c85d3380e5
glenhodges <glen@rankhigher.ca>
2018-09-14 06:49:19 -0400

    Added composer file, edited readme, changed plugin description

--- wp-cbf.php
+++ wp-cbf.php
@@ -2,17 +2,17 @@
 
 /**
  *
- * @link              http://lostwebdesigns.com
+ * @link              http://rankhigher.ca
  * @since             1.0.0
  * @package           Wp_Cbf
  *
  * @wordpress-plugin
  * Plugin Name:       WP Cleanup and Basic Options Functions
- * Plugin URI:        http://lostwebdesigns.com
+ * Plugin URI:        http://rankhigher.ca
  * Description:       Head section cleanup and many usual custom settings used on every website setup as images settings and sizes, privacy, and basic admin customizations.
- * Version:           2.2.1
- * Author:            Guillaume Kanoufi
- * Author URI:        http://lostwebdesigns.com
+ * Version:           1.0
+ * Author:            Rank Higher
+ * Author URI:        http://rankhigher.ca
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       wp-cbf
